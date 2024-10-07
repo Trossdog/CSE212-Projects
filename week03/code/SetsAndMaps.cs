c56697b5e7daccce287fae1a9b2a4d8e7b0aeb2d
Tyler Hansen <Starwars.hansen@gmail.com>
2024-10-07 13:20:52 -0600

    completed the first 4 problems of this weeks coding assignment

--- week03/code/SetsAndMaps.cs
+++ week03/code/SetsAndMaps.cs
@@ -19,10 +19,29 @@ public static class SetsAndMaps
     /// that there were no duplicates) and therefore should not be returned.
     /// </summary>
     /// <param name="words">An array of 2-character words (lowercase, no duplicates)</param>
+    
     public static string[] FindPairs(string[] words)
     {
-        // TODO Problem 1 - ADD YOUR CODE HERE
-        return [];
+        HashSet<string> seen = new HashSet<string>();
+        List<string> result = new List<string>();
+        
+        foreach (var word in words){
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            string reversedWord = new string(charArray);
+
+            if (seen.Contains(reversedWord))
+            {
+                result.Add($"{word} & {reversedWord}");
+            }
+            else
+            {
+                seen.Add(word);
+            }
+            
+        }
+        return result.ToArray();
     }
 
     /// <summary>
@@ -42,7 +61,18 @@ public static class SetsAndMaps
         foreach (var line in File.ReadLines(filename))
         {
             var fields = line.Split(",");
-            // TODO Problem 2 - ADD YOUR CODE HERE
+            var degree = fields[3].Trim();
+            
+            if (degrees.ContainsKey(degree))
+            {
+                degrees[degree]++;
+            }
+            else
+            {
+                degrees.Add(degree, 1);
+            }
+            
+
         }
 
         return degrees;
@@ -66,8 +96,32 @@ public static class SetsAndMaps
     /// </summary>
     public static bool IsAnagram(string word1, string word2)
     {
-        // TODO Problem 3 - ADD YOUR CODE HERE
-        return false;
+        word1 = word1.Replace(" ", "").ToLower();
+        word2 = word2.Replace(" ", "").ToLower();
+
+        Dictionary<char, int> letterCount1 = GetLetterCount(word1);
+        Dictionary<char, int> letterCount2 = GetLetterCount(word2);
+
+        return letterCount1.Count == letterCount2.Count && !letterCount1.Except(letterCount2).Any();
+    }
+
+    public static Dictionary<char, int> GetLetterCount(string word)
+    {
+        Dictionary<char, int> letterCount = new Dictionary<char, int>();
+
+        foreach (char letter in word)
+        {
+            if (letterCount.ContainsKey(letter))
+            {
+                letterCount[letter]++;
+            }
+            else
+            {
+                letterCount[letter] = 1;
+            }
+        }
+
+        return letterCount;
     }
 
     /// <summary>
